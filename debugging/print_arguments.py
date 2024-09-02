aa0487432bd6e83ea0e6c0291478a76de0b02b1b
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-02 08:05:49 +0000

    updating python file

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-# Vérifier s'il y a des arguments en ligne de commande
-if len(sys.argv) > 1:
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i}: {arg}")
-else:
-    print("Aucun argument fourni.")
+for arg in sys.argv:
+    print(arg)
