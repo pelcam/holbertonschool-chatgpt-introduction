11153d890048fedeec57dae96340ee788a0a53e7
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-09-02 08:12:55 +0000

    updating python file

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-import os
 
-for arg in sys.argv:
-    print(os.path.basename(arg))
+# Démarrer à l'index 1 pour éviter d'imprimer le nom du fichier
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
